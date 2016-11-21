6a40f7272ddd45f89fe3a88a378350a09ec57cdf
luis martinez <luis martinez>
2016-11-20 20:44:20 -0600

    Cadena replace

---
+++ src/mx/com/everis/tallerjava/test/TestCadena.java
@@ -0,0 +1,11 @@
+package mx.com.everis.tallerjava.test;
+
+public class TestCadena {
+
+	public static void main(String[] args) {
+		String nombre="Luis Alberto Martínez Guapo";
+		System.out.println(nombre);
+		System.out.println(nombre.replace(" ", "1"));
+	}
+	
+}
